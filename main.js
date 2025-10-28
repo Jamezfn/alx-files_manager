d3feab15d4d856fa6ea6c7d32bad866b40b3640e
Jamezfn <jamesmakachia19@gmail.com>
2025-10-28 11:15:04 +0300

    1. MongoDB utils

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-import redisClient from './utils/redis.js';
+import dbClient from './utils/db.js';
+
+const waitConnection = () => {
+  return new Promise((resolve, reject) => {
+    let i = 0;
+    const repeatFct = async () => {
+      setTimeout(() => {
+        i += 1;
+        if (i >= 10) {
+          reject(new Error('MongoDB connection timed out after 10 attempts'));
+        } else if (!dbClient.isAlive()) {
+          repeatFct();
+        } else {
+          resolve();
+        }
+      }, 1000);
+    };
+    repeatFct();
+  });
+};
 
 (async () => {
-    await redisClient.waitForConnection();
-    console.log(redisClient.isAlive());
-    console.log(await redisClient.get('myKey'));
-    await redisClient.set('myKey', 12, 5);
-    console.log(await redisClient.get('myKey'));
-
-    setTimeout(async () => {
-        console.log(await redisClient.get('myKey'));
-    }, 1000*10)
+  console.log(dbClient.isAlive());
+  try {
+    await waitConnection();
+    console.log(dbClient.isAlive());
+    console.log(await dbClient.nbUsers());
+    console.log(await dbClient.nbFiles());
+  } catch (err) {
+    console.error('Error in main:', err.message);
+  }
 })();
--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,42 @@
+import { MongoClient } from 'mongodb';
+
 class DBClient {
-	constructor() {
-		const host = process.argv.DB_HOST || 'localhost';
-		const port = process.argv.DB_PORT || 27017;
-		const database = process.env.DB_DATABASE || 'files_manager';
+        constructor() {
+                const host = process.env.DB_HOST || 'localhost';
+                const port = process.env.DB_PORT || 27017;
+                const database = process.env.DB_DATABASE || 'files_manager';
 
-		const url = `mongodb://${host}:${port}`;
+                const url = `mongodb://${host}:${port}`;
 
-		this.client = new MongoClient(url, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		});
-	}
+                this.client = new MongoClient(url);
+                this._isConnected = false;
+                this.db = null;
+                this._connectionPromise = this.client.connect()
+                        .then(() => {
+                                this._isConnected = true;
+                                this.db = this.client.db(database);
+                        })
+                        .catch((err) => {
+                                this._isConnected = false;
+                                console.error('MongoDB client connection failed:', err);
+                        });
+        }
+        isAlive() {
+                return this._isConnected;
+        }
+
+        async nbUsers() {
+                await this._connectionPromise;
+                const usersCollection = this.db.collection('users');
+                return await usersCollection.countDocuments();
+        }
+
+        async nbFiles() {
+                await this._connectionPromise;
+                const filesCollection = this.db.collection('files');
+                return await filesCollection.countDocuments();
+        }
 }
+
+const dbClient = new DBClient();
+export default dbClient;
